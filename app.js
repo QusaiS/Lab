a5d2e24eaeac61a9e73e31f757e8b67382dddb66
Qusai Saqer <70547611+QusaiS@users.noreply.github.com>
2021-05-30 21:05:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,37 @@ var userName=prompt('what is your name?  ');
 
  alert('Welcome ' + userName+' to our website'); 
  
- var important=prompt('did you creat website befor?  ');
- 
-  alert('Thats amazing Here you will learn how to draw your first website ');
+ var important=prompt('did you create website befor? if yes press 1 if no press 2 ');
+
+if ( important == 1 ){ alert('Amazing')
+}
+ else {" Thats amazing Here you will learn how to draw yourfirst website "
+ }
+var Wireframing = prompt("Are you intrested to learn about wireframing?");
   
-   var Wireframing=prompt('Are you interested to learn about Wireframing if yes ress 1')
    
-    if (Wireframing == 1) { alert('Lets go '); }  else { alert('Welcome to our website '); }
+if ( Wireframing == "yes" ) {
+  document.write("<img src='https://balsamiq.com/assets/learn/articles/mobile-web.png'>") }
+  else{
+    document.write("<h4>welcome "+userName+"</h4>");
+    }
+
+var answer = ''; 
+while (answer !== '1234') 
+if (answer == '1234'){
+  answer = prompt('Enter your password  ');}
+  else{ answer = prompt('Enter your password  (1234)');}
+
+
+
+
+  
+  
+  
+  
+  
+  
+
+
+
+
